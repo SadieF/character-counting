24b3db4deae9e89dc240f72bd50cadfc8a27b254
Sadie <sadiefreeman@gmail.com>
2018-03-14 19:57:40 +0000

    Function created for Character counting

---
+++ character-counting.js
@@ -0,0 +1,19 @@
+function countLetters(str) {
+  var arr = str.split("");
+  var result = {};
+
+  for (var i = 0; i < arr.length; i++) {
+    if(arr[i] !== " ") {
+      result[''+arr[i]] = 0;
+    for(var j = 0; j < arr.length; j++) {
+      if('' + arr[i] === '' + arr[j]) {
+        result['' + arr[j]] = result['' + arr[i]] + 1;
+        }
+       }
+      }
+    }
+    return result;
+  }
+
+
+console.log(countLetters("Lighthouse in the house"));
